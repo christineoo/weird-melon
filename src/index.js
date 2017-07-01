bf9fe111b846af5e1666bc313e59ef1097418d41
Christine Oo <ootjinvoon@gmail.com>
2017-07-01 17:41:08 +0900

    remove unused import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
-import { syncHistoryWithStore } from 'react-router-redux';
 import { hashHistory } from 'react-router';
 import Root from './containers/Root'
 
